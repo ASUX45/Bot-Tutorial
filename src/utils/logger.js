e2ab9d54dd58156425ff52479546a344c3ec65c5
ASUX45 <76892191+ASUX45@users.noreply.github.com>
2021-01-27 16:21:42 -0300

    Create logger.js

---
+++ src/utils/logger.js
@@ -0,0 +1,16 @@
+const moment = require('moment');
+
+ module.exports = {
+     async sucess(content) {
+         console.log(`[${moment().format("DD-MM-YYYY HH:mm")}] - Sucesso` + content
+         );
+     },
+     async error(content) {
+         console.log(`[${moment().format("DD-MM-YYYY HH:mm")} - ERRO]` + content
+         );
+     },
+     async warn(content) {
+         console.log(`[${moment().format("DD-MM-YYYY HH:mm")} - WARN]` + content
+         );
+     },
+ };
